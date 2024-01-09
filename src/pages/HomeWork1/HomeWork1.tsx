aabc062fcad9ba6ed16ad4e05fd0b2817b5e6e36
CORP\margarita.suvorova <Margarita.Suvorova_ext@leroymerlin.ru>
2024-01-09 21:11:24 +0300

    homework1

--- src/pages/HomeWork1/HomeWork1.tsx
+++ src/pages/HomeWork1/HomeWork1.tsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+import style from './styles.module.css';
+
+
 export const HomeWork1 = () => {
-  return <div>Решение домашнего задания №1</div>;
+  const [number, setNumber] = useState<number>(1);
+
+  const runHandler = () => {
+    const calcNumber = Math.floor( Math.random() * 6 ) + 1;
+    setNumber(calcNumber);
+  };
+
+  return <div>
+    <h3>Решение домашнего задания №1</h3>
+    <div className={style.cube}>{number}</div>
+    <button onClick={runHandler}>Run</button>
+  </div>;
 };
